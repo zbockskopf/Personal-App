f22e9b55ea7fdc8710eab1910c581ebbb57954a5
Zach Bockskopf <zbockskopf@icloud.com>
2017-12-24 03:12:16 -0600

    Add spendings sheet
    added a spendings viewcontroller that shows all the spendings. The add controller adds the transaction to the spendings parent in the database and adds the amount to the specified category

---
+++ Personal Budget/View Controllers/SpendingsController.swift
@@ -0,0 +1,25 @@
+//
+//  SpendingsController.swift
+//  Personal Budget
+//
+//  Created by Zach Bockskopf on 12/22/17.
+//  Copyright © 2017 Zach Bockskopf. All rights reserved.
+//
+
+
+
+/* This controller is used to show all the transactions for the current month */
+
+import UIKit
+import FirebaseDatabase
+import FirebaseAuth
+
+class SpendingsController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
